3609a559da073d1c1bd48f4d8cdf524da2fd11f8
Bagas Dewantoro <bagas.dewantoro98@gmail.com>
2020-10-26 13:04:53 +0700

    tugas 3 mas

--- Tugas3_123200107.cpp
+++ Tugas3_123200107.cpp
@@ -20,6 +20,7 @@ int main()
 		cout << "1. Algoritma dan Pemrograman\n";
 		cout << "2. Kalkulus\n";
 		cout << "Pilih : "; cin >> pil;
+
 		if(pil == 1){
 			cout << "Nilai Harian\t : "; cin >> harian;
 			cout << "Nilai UTS\t : "; cin >> uts;
@@ -38,14 +39,17 @@ int main()
 			cout << "Nilai UAS\t : "; cin >> uas;
 			cout << endl;
 			hasil = (harian + uts + uas)/3;
+
 			if(hasil >= 80){
 				cout << "Anda lulus Kalkulus dengan nilai " << hasil << endl;
 			}else{
 				cout << "Anda tidak lulus Kalkulus dengan nilai " << hasil << endl;
 			}
+
 		}else{
 			cout << "Input anda salah!\n";
 		}
+		
 	}else if(uname == "admin" && pword != 123){
 		cout << "Password anda salah!" << endl;
 	}else if(pword == 123 && uname != "admin"){
